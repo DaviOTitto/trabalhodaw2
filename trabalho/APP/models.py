35d086215b7e6399452aa3666d35396d17a18e4a
DaviOTitto <davioliveiratito@gmail.com>
2024-04-11 11:51:42 -0300

    salvar

--- trabalho/APP/models.py
+++ trabalho/APP/models.py
@@ -14,7 +14,33 @@ class Teste(models.Model):
     texto = models.TextField("texto",null = True)
     interio = models.IntegerField("inteiro",blank=True)
     Boolean = models.BooleanField("booelan",blank=True,null=True)
-    #lista = models.CharField("escolha", choices = options_choices)
-    escolha_radio = models.IntegerField("escolharadio", null =True)
+    lista = models.CharField("escolha", choices = options_choices)
+    escolha_radio = models.CharField("escolharadio", null =True)
+
+
+
+class Doador(models.Model):
+    codigo = models.AutoField("alto completo",null=False,primary_key=True)
+    nome = models.CharField("Nome",blank=True,max_length=200)
+    cpf = models.CharField("CPF",blank=True,max_length=11)
+    contato = models.CharField("numero",blank=True,max_length=12)
+    tipo_sanguineo = models.CharField("tipo sanguineo",blank=True,max_length=4)
+    rh = models.CharField("RH",blank=True,max_length=1)
+    tipo_rh_corretos = models.BooleanField("RH corretos",blank=True)
+    situacao = models.CharField("situcao", blank=True)
+    class Meta:
+        ordering = ['codigo']
+        verbose_name = 'Doador'
+class Doacao(models.Model):
+    codigo = models.AutoField("alto completo",null=False,primary_key=True)
+    Data = models.DateField("Data",auto_now_add=True,auto_now=False)
+    Hora = models.TimeField("time",auto_now_add=True,auto_now=False)
+    volume = models.DecimalField("volume",decimal_places = 2 , max_digits = 7)
+    situacao = models.CharField("situcao", blank=True)
+    codigo_doador = models.ForeignKey(Doador,db_column='codigo_doador',related_name='codigo_do',on_delete=models.CASCADE)  
+    class Meta:
+        ordering = ['codigo']
+        verbose_name = 'Doador'
+
 
 
