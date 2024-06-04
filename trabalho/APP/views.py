6a370f52e458cb18fe541fe59565002b1cf78424
DaviOTitto <davioliveiratito@gmail.com>
2024-06-04 12:05:56 -0300

    ta dando erro

--- trabalho/APP/views.py
+++ trabalho/APP/views.py
@@ -62,14 +62,71 @@ def teste(request):
 mar6 = TemplateView.as_view(template_name='resultado>6.html')
 men6 = TemplateView.as_view(template_name='resultado<6.html')
 
-def Doador_list(request):
-    object_list = Doador.objects.all().order_by('codigo')
-    print(object_list)
-    # pesquisa na lista de clientes pelo nome do cliente        
-    search = request.GET.get('search_box')
-    if search:
-        object_list = object_list.filter(nome=search)
+def altera_doador(request,pk):
+    order_forms = Doador()
+    doador = get_object_or_404(Doador,pk=pk)                        
+    doador =  Doador.objects.all().order_by('pk')                   
+    i = 1
+    if request.method == 'GET':
+        forms = DoadorForm(instance=doador,prefix='main')
+    else:
+        forms = DoadorForm(request.POST,request.FILES,instance=venda,prefix='main')
+        if forms.is_valid() and formset.is_valid():
+            forms = forms.save(commit=False)
+            # apagado user.profile da linha 163 , Davi Oliveira Tito 02/05/2022
+            i = 0
+            for inline_form in formset:
+                inline_form = inline_form.save(commit=False)
+                i += 1
+            forms.save()
+            formset.save()
+            return HttpResponseRedirect(resolve_url('core:detalhe_doador',forms.pk))
+        else:
+            messages.warning(request,'Erro interno ao alterar a Doador!')
+    context = {
+        'forms':forms,
+    }
+    return render(request,'core/venda/altera_doador.html',context)
 
+def deleta_doador(request,pk):
+    get_object_or_404(Doador,pk=pk).delete()
+    return redirect('core:listar_vendas')
+  
+
+def Doador_list(request):
+    # Obtém todos os objetos do modelo Doador 
+    object_list = Doador.objects.all().order_by('codigo')
+    doador_Ver = Doador()
+    forms = None  # Defina a variável fora do bloco condicional
+
+    if request.method == 'POST':
+        forms = DoadorForm(request.POST, request.FILES,
+                       instance=doador_Ver, prefix='main')
+    # Processa a pesquisa (se houver)
+    nome = request.GET.get('nome')
+    cpf = request.GET.get('cpf')
+    contato = request.GET.get('contato')
+    tipo_sanguineo = request.GET.get('tipo_sanguineo')
+    rh = request.GET.get('rh')
+    tipo_rh_corretos = request.GET.get('tipo_rh_corretos')
+    situacao = request.GET.get('situacao')
+
+    if nome:
+        object_list = object_list.filter(nome__icontains=nome)
+    if cpf:
+        object_list = object_list.filter(cpf__icontains=cpf)
+    if contato:
+        object_list = object_list.filter(contato__icontains=contato)
+    if tipo_sanguineo:
+        object_list = object_list.filter(tipo_sanguineo__icontains=tipo_sanguineo)
+    if rh:
+        object_list = object_list.filter(rh__icontains=rh)
+    if tipo_rh_corretos:
+        object_list = object_list.filter(tipo_rh_corretos__icontains=tipo_rh_corretos)
+    if situacao:
+        object_list = object_list.filter(situacao__icontains=situacao)
+
+    # Configura a paginação
     paginator = Paginator(object_list, 20)
     page = request.GET.get('page', 1)
     try:
@@ -80,11 +137,19 @@ def Doador_list(request):
         doador = paginator.page(paginator.num_pages)
 
     context = {
-      'object_list':object_list,
-      'doador':doador}
-    
-    return render(request,'lista_doador.html',context)
+        'forms' : forms,
+        'object_list': object_list,
+        'doador': doador,
+        'nome': nome,
+        'cpf': cpf,
+        'contato': contato,
+        'tipo_sanguineo': tipo_sanguineo,
+        'rh': rh,
+        'tipo_rh_corretos': tipo_rh_corretos,
+        'situacao': situacao,
+    }
 
+    return render(request, 'lista_doador.html', context)
 
 def formulario(request):
   if request.method == 'post':  
